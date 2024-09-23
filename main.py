6a224fbf3f0db645aa7cad64c0aaed1980b520ce
Justin Myers <jmyers.sde@gmail.com>
2024-09-22 23:10:30 -0400

    First commit on first feature/update-message branch

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+
+def hello_world():
+    return "Hello from Justin Myers!"
+
+if __name__ == "__main__":
+    print(hello_world())
---
+++ test_main.py
@@ -0,0 +1,11 @@
+# test_main.py
+
+import unittest
+from main import hello_world
+
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello from Justin Myers!")
+
+if __name__ == '__main__':
+    unittest.main()
